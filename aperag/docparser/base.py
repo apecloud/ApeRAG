4676e01a9dc8e112b6719b80231a1590ebce29d0
x.zhou <x.zhou@apecloud.com>
2025-05-18 14:43:17 +0800

    docparse package

---
+++ aperag/docparser/base.py
@@ -0,0 +1,61 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Any
+
+from pydantic import BaseModel, Field
+
+
+class FallbackError(Exception):
+    pass
+
+
+class Part(BaseModel):
+    content: str | None = Field(
+        default=None,
+        description="The parsed content. If None, it means that information extraction has not been performed on this node yet.",
+    )
+    metadata: dict[str, Any] = Field(default_factory=dict)
+
+
+class MarkdownPart(Part):
+    pass
+
+
+class TextPart(Part):
+    pass
+
+
+class TitlePart(TextPart):
+    level: int
+
+
+class CodePart(Part):
+    lang: str | None = None
+
+
+class MediaPart(Part):
+    url: str
+    mime_type: str | None = None
+
+
+class ImagePart(MediaPart):
+    alt_text: str | None = None
+    title: str | None = None
+
+
+class AssetBinPart(BaseModel):
+    asset_id: str
+    data: bytes
+    mime_type: str | None = None
+    metadata: dict[str, Any] = Field(default_factory=dict)
+
+
+class BaseParser(ABC):
+    @abstractmethod
+    def supported_extensions(self) -> list[str]: ...
+
+    def accept(self, extension: str) -> bool:
+        return extension.lower() in self.supported_extensions()
+
+    @abstractmethod
+    def parse_file(self, path: Path, metadata: dict[str, Any] = {}, **kwargs) -> list[Part]: ...
---
+++ aperag/docparser/utils.py
@@ -0,0 +1,38 @@
+import os
+import shutil
+import subprocess
+from pathlib import Path
+
+
+def get_soffice_cmd() -> str | None:
+    return shutil.which("soffice")
+
+
+def convert_office_doc(input_path: Path, output_dir: Path, target_format: str) -> Path:
+    soffice_cmd = get_soffice_cmd()
+    if soffice_cmd is None:
+        raise RuntimeError("soffice command not found")
+
+    if not input_path.exists:
+        raise FileNotFoundError(f"input file {input_path} not exist")
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    cmd = [
+        soffice_cmd,
+        "--headless",
+        "--norestore",
+        "--convert-to",
+        target_format,
+        "--outdir",
+        str(output_dir),
+        str(input_path),
+    ]
+
+    process = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if process.returncode != 0:
+        raise RuntimeError(
+            f'convert failed, cmd: "{" ".join(cmd)}", output: {process.stdout.decode()}, error: {process.stderr.decode()}'
+        )
+
+    return output_dir / f"{input_path.stem}.{target_format}"
