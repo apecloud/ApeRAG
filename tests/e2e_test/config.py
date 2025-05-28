179a415e8d9fb1dc48a8a73559b45ad323451fd4
Guo Ziang <ziang.gza@apecloud.com>
2025-05-28 23:36:44 +0800

    chore: add collection e2e tests

---
+++ tests/e2e_test/config.py
@@ -0,0 +1,27 @@
+# Copyright 2025 ApeCloud, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+API_BASE_URL = "http://localhost:8000"
+API_KEY = "sk-40e23724fff1459f8e8ab67aac4f9de1"
+
+EMBEDDING_MODEL_PROVIDER = "siliconflow"
+EMBEDDING_MODEL_NAME = "BAAI/bge-large-zh-v1.5"
+EMBEDDING_MODEL_CUSTOM_PROVIDER = "openai"
+
+COMPLETION_MODEL_PROVIDER = "openrouter"
+COMPLETION_MODEL_NAME = "deepseek/deepseek-chat-v3-0324:free"
+COMPLETION_MODEL_CUSTOM_PROVIDER = "openrouter"
+
+RERANK_MODEL_PROVIDER = "siliconflow"
+RERANK_MODEL_NAME = "BAAI/bge-large-zh-1.5"
