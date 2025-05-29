f772e46309c975cc72263db8f6235d2cd8964fd3
Guo Ziang <ziang.gza@apecloud.com>
2025-05-29 23:58:47 +0800

    chore: remove question related code

--- aperag/pipeline/common_pipeline.py
+++ aperag/pipeline/common_pipeline.py
@@ -12,14 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import asyncio
 import logging
-import random
 
 from langchain_core.prompts import PromptTemplate
 
 from aperag.llm.prompts import COMMON_FILE_TEMPLATE
-from aperag.pipeline.base_pipeline import RELATED_QUESTIONS, Message, Pipeline
+from aperag.pipeline.base_pipeline import Message, Pipeline
 from aperag.utils.utils import now_unix_milliseconds
 
 logger = logging.getLogger(__name__)
@@ -49,20 +47,9 @@ class CommonPipeline(Pipeline):
         log_prefix = f"{message_id}|{message}"
         logger.info("[%s] start processing", log_prefix)
 
-        related_questions = set()
         response = ""
 
         need_generate_answer = True
-        need_related_question = True
-
-        if self.oops != "":
-            response = self.oops
-            yield self.oops
-            need_generate_answer = False
-        if self.welcome_question != []:
-            related_questions.update(self.welcome_question)
-            if len(self.welcome_question) >= 3:
-                need_related_question = False
 
         # TODO: divide file_content into several parts and call API separately.
         context = file if file else ""
@@ -70,10 +57,6 @@ class CommonPipeline(Pipeline):
 
         if len(context) > self.context_window - 500:
             context = context[: self.context_window - 500]
-
-        if self.use_related_question and need_related_question:
-            related_question_prompt = self.related_question_prompt.format(query=message, context=context)
-            related_question_task = asyncio.create_task(self.generate_related_question(related_question_prompt))
 
         if need_generate_answer:
             history = [{"role": "system", "content": self.prompt.format(query="")}]
@@ -108,14 +91,6 @@ class CommonPipeline(Pipeline):
 
                 await self.add_ai_message(message, message_id, response, references=[], urls=[])
                 logger.info("[%s] add ai message end and the pipeline is succeed", log_prefix)
-
-            if self.use_related_question:
-                if need_related_question:
-                    related_question_generate = await related_question_task
-                    related_questions.update(related_question_generate)
-                related_questions = list(related_questions)
-                random.shuffle(related_questions)
-                yield RELATED_QUESTIONS + str(related_questions[:3])
 
 
 async def create_common_pipeline(**kwargs) -> CommonPipeline:
