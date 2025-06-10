2ca3fa00acaed574b58ee7f20bfc1499b1217eec
Guo Ziang <ziang.gza@apecloud.com>
2025-06-10 14:19:12 +0800

    chore: tidy up

--- aperag/migration/versions/20250610141055-6b261afb99c8.py
+++ aperag/migration/versions/20250610141055-6b261afb99c8.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: f88ca40f70ed
+Revision ID: 6b261afb99c8
 Revises: 
-Create Date: 2025-06-10 13:18:40.354286
+Create Date: 2025-06-10 14:10:55.477279
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'f88ca40f70ed'
+revision: str = '6b261afb99c8'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
