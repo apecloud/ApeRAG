7c7d72d05387a30c420b8d0bc576f4e17908bfdf
Guo Ziang <ziang.gza@apecloud.com>
2025-06-10 16:18:45 +0800

    chore: tidy up

--- scripts/start-api.sh
+++ scripts/start-api.sh
@@ -5,13 +5,6 @@ set -o pipefail
 set -o nounset
 
 
-python3 manage.py migrate
+alembic -c aperag/alembic.ini upgrade head
 
-if [ -f "static/web/index.html" ]; then
-  cp static/web/index.html aperag/templates/404.html
-fi
-
-# uvicorn supports multiple workers
-exec uvicorn config.asgi:application --host 0.0.0.0 --log-config scripts/uvicorn-log-config.yaml
-
-#exec daphne config.asgi:application -b 0.0.0.0
+exec uvicorn aperag.app:app --host 0.0.0.0 --log-config scripts/uvicorn-log-config.yaml
