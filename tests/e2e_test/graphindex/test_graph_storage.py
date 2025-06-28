f1fa767f729103181d1079a96f4e73eeae5d43a5
earayu <earayu@163.com>
2025-06-28 17:30:00 +0800

    feat: use match for has_node

--- tests/e2e_test/graphindex/test_graph_storage.py
+++ tests/e2e_test/graphindex/test_graph_storage.py
@@ -678,7 +678,7 @@ class GraphStorageTestSuite:
         assert isinstance(all_labels, list)
 
         # Check that some nodes from our test data are present
-        sample_entities = get_random_sample(graph_data["nodes"], max_size=100, min_size=60)
+        sample_entities = get_random_sample(graph_data["nodes"], max_size=50, min_size=30)
         found_entities = 0
         
         for entity in sample_entities:
