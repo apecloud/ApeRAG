00ead9b75165b564cb73b91f920fbd22eaa440c9
申栋栋 <sailwebs@apecloud.com>
2025-08-14 00:42:00 +0800

    fix: auth

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -31,10 +31,8 @@ export default async function RootLayout({
   try {
     const res = await apiServer.defaultApi.userGet();
     user = res.data;
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (err) {
-    
-  }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (err) {}
 
   return (
     <html lang="en" suppressHydrationWarning>
