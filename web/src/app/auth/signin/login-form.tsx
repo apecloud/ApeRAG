f70e4e191521e49f9a91315e53160756d25c9663
申栋栋 <sailwebs@apecloud.com>
2025-08-13 22:14:24 +0800

    fix: login

---
+++ web/src/app/auth/signin/login-form.tsx
@@ -0,0 +1,142 @@
+'use client';
+
+import { Config } from '@/api';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardContent } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import {
+  signInLocalSchema,
+  useGlobalContext,
+} from '@/hooks/use-global-context';
+import { apiClient } from '@/lib/api/client';
+import { cn } from '@/lib/utils';
+import { useSearchParams } from 'next/navigation';
+import { FormEvent, useCallback, useEffect, useMemo, useState } from 'react';
+import { FaGithub, FaGoogle } from 'react-icons/fa6';
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<'div'>) {
+  const [config, setConfig] = useState<Config>({
+    login_methods: ['local'],
+  });
+  const searchParams = useSearchParams();
+  const redirectTo = searchParams.get('callbackUrl') || '/';
+  const { signIn } = useGlobalContext();
+
+  const hasSocialLogin = useMemo(() => {
+    return config.login_methods?.some((method) =>
+      ['google', 'github'].includes(method),
+    );
+  }, [config.login_methods]);
+
+  const handleSignInLocal = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const formData = new FormData(e.currentTarget);
+      const payload = Object.fromEntries(formData.entries());
+      const { data, error } = signInLocalSchema.safeParse(payload);
+
+      if (error) {
+        return;
+      }
+
+      await signIn({
+        type: 'local',
+        data: data,
+        redirectTo,
+      });
+    },
+    [redirectTo, signIn],
+  );
+
+  useEffect(() => {
+    apiClient.defaultApi.configGet().then((res) => {
+      setConfig(res.data);
+    });
+  }, []);
+
+  return (
+    <div className={cn('flex flex-col gap-6', className)} {...props}>
+      <Card>
+        <CardContent>
+          <div className="mb-4 text-center text-xl font-bold">Welcome back</div>
+          {hasSocialLogin && (
+            <>
+              <div className="text-muted-foreground mb-6 text-center text-sm">
+                Login with your Github or Google account
+              </div>
+              <div className="flex flex-col gap-4 text-sm">
+                <Button
+                  variant="outline"
+                  className="w-full"
+                  onClick={() => signIn({ type: 'github', redirectTo })}
+                >
+                  <FaGithub />
+                  Login with Github
+                </Button>
+                <Button
+                  variant="outline"
+                  className="w-full"
+                  onClick={() => signIn({ type: 'google', redirectTo })}
+                >
+                  <FaGoogle />
+                  Login with Google
+                </Button>
+              </div>
+              <div className="after:border-border relative my-6 text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                <span className="bg-card text-muted-foreground relative z-10 px-2">
+                  Or continue with
+                </span>
+              </div>
+            </>
+          )}
+
+          <form onSubmit={handleSignInLocal}>
+            <div className="grid gap-6">
+              <div className="grid gap-6">
+                <div className="grid gap-3">
+                  <Label htmlFor="username">Username</Label>
+                  <Input id="username" name="username" required />
+                </div>
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="#"
+                      className="ml-auto text-sm underline-offset-4 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input
+                    id="password"
+                    name="password"
+                    type="password"
+                    required
+                  />
+                </div>
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+              </div>
+              <div className="text-center text-sm">
+                Don&apos;t have an account?{' '}
+                <a href="#" className="underline underline-offset-4">
+                  Sign up
+                </a>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+      {/* <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a>{' '}
+        and <a href="#">Privacy Policy</a>.
+      </div> */}
+    </div>
+  );
+}
--- web/src/app/global-provider.tsx
+++ web/src/app/global-provider.tsx
@@ -1,15 +1,68 @@
 'use client';
 
-import { GlobalContext, GlobalContextProps } from '@/hooks/use-global-context';
+import {
+  GlobalContext,
+  signInLocalSchema,
+  SignInOptions,
+} from '@/hooks/use-global-context';
+
+import { User } from '@/api';
+import { apiClient } from '@/lib/api/client';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 export const GlobalProvider = ({
   user,
   children,
-}: GlobalContextProps & { children: React.ReactNode }) => {
+}: {
+  user?: User;
+  children?: React.ReactNode;
+}) => {
+  const [_user, setUser] = useState<User | undefined>(user);
+
+  const router = useRouter();
+  const handleSignIn = async (options?: SignInOptions) => {
+    if (options === undefined) {
+      const callbackUrl = encodeURIComponent(window.location.href);
+      router.push(`/auth/signin?callbackUrl=${callbackUrl}`);
+      return;
+    }
+    if (options.type === 'local') {
+      const { data } = signInLocalSchema.safeParse(options.data);
+      if (!data) return;
+      const res = await apiClient.defaultApi.loginPost({
+        login: {
+          username: data.username,
+          password: data.password,
+        },
+      });
+      if (res.status === 200) {
+        setUser(res.data);
+        router.push(options.redirectTo);
+      }
+    }
+
+    if (options.type === 'google') {
+    }
+
+    if (options.type === 'github') {
+    }
+  };
+
+  const handleSignOut = async () => {
+    const res = await apiClient.defaultApi.logoutPost();
+    if (res.status === 200) {
+      setUser(undefined);
+      router.refresh();
+    }
+  };
+
   return (
     <GlobalContext.Provider
       value={{
-        user,
+        user: _user,
+        signIn: handleSignIn,
+        signOut: handleSignOut,
       }}
     >
       {children}
