8436c1589315807c94bd3d4bdfa194df65c10c1c
申栋栋 <sailwebs@apecloud.com>
2025-08-16 15:33:14 +0800

    fix: remove lib from gitignore

---
+++ web/src/lib/utils.ts
@@ -0,0 +1,24 @@
+import type { ClassValue } from 'clsx';
+import { clsx } from 'clsx';
+import ColorHash from 'color-hash';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+// export function toJson<T>(obj: T): T {
+//   return JSON.parse(JSON.stringify(obj));
+// }
+export const toJson = <T>(obj: T): T => {
+  return JSON.parse(JSON.stringify(obj));
+};
+
+export const objectKeys = <T extends object>(
+  obj?: T,
+): Array<keyof T> | undefined => {
+  if (obj === undefined) return;
+  return Object.keys(obj) as Array<keyof T>;
+};
+
+export const colorHash = new ColorHash();
