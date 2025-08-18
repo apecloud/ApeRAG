20c3b822fd3f92aa5328ec93ae8725208d79fb64
申栋栋 <sailwebs@apecloud.com>
2025-08-18 13:59:08 +0800

    fix: document list

---
+++ web/src/app/workspace/collections/[collectionId]/files/page.tsx
@@ -0,0 +1,48 @@
+import {
+  PageContainer,
+  PageContent,
+  PageHeader,
+} from '@/components/page-container';
+import { getServerApi } from '@/lib/api/server';
+import { toJson } from '@/lib/utils';
+import { CollectionHeader } from '../collection-header';
+import { FilesTable } from './files-table';
+
+export default async function Page({
+  params,
+}: Readonly<{
+  params: Promise<{ collectionId: string }>;
+}>) {
+  const { collectionId } = await params;
+  const serverApi = await getServerApi();
+
+  const [collectionRes, documentsRes] = await Promise.all([
+    serverApi.defaultApi.collectionsCollectionIdGet({
+      collectionId,
+    }),
+    serverApi.defaultApi.collectionsCollectionIdDocumentsGet({
+      collectionId,
+    }),
+  ]);
+
+  return (
+    <PageContainer>
+      <PageHeader
+        breadcrumbs={[
+          {
+            title: 'Collections',
+            href: '/workspace/collections',
+          },
+          {
+            title: 'File Explorer',
+          },
+        ]}
+      />
+      <CollectionHeader collection={toJson(collectionRes.data)} />
+
+      <PageContent className='pt-0'>
+        <FilesTable data={toJson(documentsRes.data.items || [])} />
+      </PageContent>
+    </PageContainer>
+  );
+}
