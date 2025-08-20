f3832aa65ef2eb9619f7bc536c94b19562c3c3c2
申栋栋 <sailwebs@apecloud.com>
2025-08-20 18:51:38 +0800

    fix: rebase from main

--- web/src/app/theme-provider.tsx
+++
@@ -1,9 +0,0 @@
-import { ThemeProvider as NextThemeProvider } from 'next-themes';
-import * as React from 'react';
-
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemeProvider>) {
-  return <NextThemeProvider {...props}>{children}</NextThemeProvider>;
-}
