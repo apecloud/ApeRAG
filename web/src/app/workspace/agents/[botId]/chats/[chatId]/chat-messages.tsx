73f2c811270f3b0a649315d6ad7354e40ea42e82
申栋栋 <sailwebs@apecloud.com>
2025-08-27 01:54:12 +0800

    fix: page title

--- web/src/app/workspace/agents/[botId]/chats/[chatId]/chat-messages.tsx
+++ web/src/app/workspace/agents/[botId]/chats/[chatId]/chat-messages.tsx
@@ -189,7 +189,7 @@ export const ChatMessages = ({ chat }: { chat?: ChatDetails }) => {
   // }, [loadMessages]);
 
   return (
-    <div className="pb-70">
+    <div className="pb-70 flex flex-col gap-6">
       {messages.map((parts, index) => {
         const isAI = parts.some((part) => part.role === 'ai');
         const isLoading = loading && index + 1 === messages.length;
