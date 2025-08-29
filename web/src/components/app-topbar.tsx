c124c9bc5825a047f246b56e3e5789ea950ce46b
申栋栋 <sailwebs@apecloud.com>
2025-08-29 22:38:55 +0800

    fix: i18n support

--- web/src/components/app-topbar.tsx
+++ web/src/components/app-topbar.tsx
@@ -11,6 +11,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import {
+  Check,
   ChevronsUpDown,
   CircleQuestionMark,
   Moon,
@@ -28,6 +29,8 @@ import { useAppContext } from '@/components/providers/app-provider';
 import { cn } from '@/lib/utils';
 
 import { useIsMobile } from '@/hooks/use-mobile';
+import { setLocale } from '@/services/cookies';
+import { useLocale } from 'next-intl';
 import { FaGithub } from 'react-icons/fa6';
 import { NavigationMenu, NavigationMenuList } from './ui/navigation-menu';
 import { UserAvatar, UserAvatarProfile } from './user-avatar';
@@ -60,6 +63,8 @@ export const AppUserDropdownMenu = () => {
   const { user, signIn, signOut } = useAppContext();
   const username = user?.username || user?.email?.split('@')[0];
   const isMobile = useIsMobile();
+  const locale = useLocale();
+
   if (!user) {
     return (
       <Button
@@ -93,12 +98,31 @@ export const AppUserDropdownMenu = () => {
       <DropdownMenuContent
         className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
         align="end"
-        side={isMobile ? 'bottom' : 'right'}
+        side="bottom"
         sideOffset={isMobile ? 4 : 12}
       >
         <DropdownMenuLabel className="font-normal">
           <UserAvatarProfile user={user} />
         </DropdownMenuLabel>
+        <DropdownMenuSeparator />
+
+        <DropdownMenuGroup>
+          <DropdownMenuItem onClick={() => setLocale('en-US')}>
+            <Check
+              data-active={locale === 'en-US'}
+              className="opacity-0 data-[active=true]:opacity-100"
+            />
+            English
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setLocale('zh-CN')}>
+            <Check
+              data-active={locale === 'zh-CN'}
+              className="opacity-0 data-[active=true]:opacity-100"
+            />
+            简体中文
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+
         <DropdownMenuSeparator />
 
         <DropdownMenuGroup>
