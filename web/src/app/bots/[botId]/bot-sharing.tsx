4a647666a14b4a9b4dc221f27a3c6def8da78b06
申栋栋 <sailwebs@apecloud.com>
2025-09-24 23:59:47 +0800

    fix: department select

---
+++ web/src/app/bots/[botId]/bot-sharing.tsx
@@ -0,0 +1,102 @@
+'use client';
+
+import { Department } from '@/api';
+import { useBotContext } from '@/components/providers/bot-provider';
+import {
+  TreeMultipleSelect,
+  TreeSelectItem,
+} from '@/components/tree-multiple-select';
+
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import { Label } from '@/components/ui/label';
+import { apiClient } from '@/lib/api/client';
+import { Slot } from '@radix-ui/react-slot';
+import { useTranslations } from 'next-intl';
+import { useCallback, useEffect, useState } from 'react';
+
+export const BotSharing = ({ children }: { children?: React.ReactNode }) => {
+  const [selectedDepartents, setSelectedDepartents] = useState<string[]>([]);
+  const [departments, setDepartents] = useState<Department[]>([]);
+  const { bot } = useBotContext();
+  const page_bot = useTranslations('page_bot');
+  const [sharingVisible, setSharingVisible] = useState<boolean>(false);
+
+  const common_action = useTranslations('common.action');
+  // const common_tips = useTranslations('common.tips');
+
+  const handleSharing = useCallback(async () => {
+    if (!bot?.id) return;
+    const res = await apiClient.defaultApi.botsBotIdSharingPost({
+      botId: bot.id,
+      botPublishRequest: {
+        group_ids: selectedDepartents,
+      },
+    });
+  }, [bot?.id, selectedDepartents]);
+
+  const getDepartments = useCallback(async () => {
+    const res = await apiClient.defaultApi.departmentsGet();
+    setDepartents(res.data.items || []);
+  }, []);
+
+  useEffect(() => {
+    if (sharingVisible && departments.length === 0) {
+      getDepartments();
+    }
+  }, [departments.length, getDepartments, sharingVisible]);
+
+  return (
+    <Dialog open={sharingVisible} onOpenChange={() => setSharingVisible(false)}>
+      <DialogTrigger asChild>
+        <Slot
+          onClick={(e) => {
+            setSharingVisible(true);
+            e.preventDefault();
+          }}
+        >
+          {children}
+        </Slot>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{page_bot('share_settings')}</DialogTitle>
+          <DialogDescription>
+            {page_bot('share_settings_tips')}
+          </DialogDescription>
+        </DialogHeader>
+        <div>
+          <div className="mb-2 flex h-4 flex-row items-center gap-2 text-sm">
+            <Label>
+              {page_bot('shared_department')} ({selectedDepartents.length})
+            </Label>
+          </div>
+          <TreeMultipleSelect
+            options={departments as TreeSelectItem[]}
+            className="h-60 overflow-auto rounded-md border"
+            onValuesChange={(v) => setSelectedDepartents(v)}
+          />
+        </div>
+        <DialogFooter>
+          <Button variant="outline" onClick={() => setSharingVisible(false)}>
+            {common_action('cancel')}
+          </Button>
+          <Button
+            disabled={selectedDepartents.length === 0}
+            onClick={() => handleSharing()}
+          >
+            {common_action('continue')}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- web/src/app/bots/bot-list.tsx
+++ web/src/app/bots/bot-list.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Bot } from '@/api';
+import { Bot, SharedBot } from '@/api';
 import { FormatDate } from '@/components/format-date';
 import { Button } from '@/components/ui/button';
 import {
@@ -11,75 +11,131 @@ import {
   CardTitle,
 } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { apiClient } from '@/lib/api/client';
+import { useInterval } from 'ahooks';
 import { Calendar, Plus } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
-export const BotList = ({ bots }: { bots: Bot[] }) => {
-  const [searchValue, setSearchValue] = useState<string>('');
+const BotCard = ({ bot }: { bot: Bot }) => {
   const page_bot = useTranslations('page_bot');
   return (
-    <>
-      <div className="mb-4 flex flex-row items-center">
-        <div>
-          <Input
-            placeholder={page_bot('search')}
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.currentTarget.value)}
-          />
-        </div>
-        <div className="ml-auto flex items-center gap-2">
-          <Button asChild>
-            <Link href="/bots/new">
-              <Plus /> {page_bot('new_bot')}
-            </Link>
-          </Button>
-        </div>
-      </div>
+    <Link key={bot.id} href={`/bots/${bot.id}/chats`}>
+      <Card className="hover:bg-accent/30 cursor-pointer gap-2 rounded-md">
+        <CardHeader className="px-4">
+          <CardTitle className="h-5 truncate">{bot.title}</CardTitle>
+        </CardHeader>
+        <CardDescription className="mb-4 truncate px-4">
+          {bot.description || page_bot('no_description_available')}
+        </CardDescription>
+        <CardFooter className="justify-between px-4 text-xs">
+          <div className="text-muted-foreground">
+            {bot.created && (
+              <div className="flex items-center gap-2">
+                <Calendar className="size-3" />
+                <FormatDate datetime={new Date(bot.created)} />
+              </div>
+            )}
+          </div>
+        </CardFooter>
+      </Card>
+    </Link>
+  );
+};
 
-      {bots.length === 0 ? (
-        <div className="bg-accent/50 text-muted-foreground rounded-lg py-40 text-center">
-          {page_bot('no_bots_found')}
+const BotsCardList = ({
+  bots,
+  searchValue,
+}: {
+  bots: Bot[];
+  searchValue: string;
+}) => {
+  const page_bot = useTranslations('page_bot');
+  return bots.length === 0 ? (
+    <div className="bg-accent/50 text-muted-foreground rounded-lg py-40 text-center">
+      {page_bot('no_bots_found')}
+    </div>
+  ) : (
+    <div className="sm:grid-col-1 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+      {bots
+        .filter((bot) => {
+          if (searchValue === '') return true;
+          return (
+            bot.title?.match(new RegExp(searchValue)) ||
+            bot.description?.match(new RegExp(searchValue))
+          );
+        })
+        .map((bot) => {
+          return <BotCard key={bot.id} bot={bot} />;
+        })}
+    </div>
+  );
+};
+
+export const BotList = ({
+  bots: initBots,
+  sharedBots: initSharedBots,
+}: {
+  bots: Bot[];
+  sharedBots: SharedBot[];
+}) => {
+  const [searchValue, setSearchValue] = useState<string>('');
+  const page_bot = useTranslations('page_bot');
+
+  const [bots, setBots] = useState<Bot[]>(initBots);
+
+  const [sharedBots, setSharedBots] = useState<SharedBot[]>(initSharedBots);
+
+  const getBots = useCallback(async () => {
+    const res = await apiClient.defaultApi.botsGet();
+    setBots(res.data.items || []);
+  }, []);
+
+  const getSharedBots = useCallback(async () => {
+    const res = await apiClient.defaultApi.marketplaceBotsGet();
+    setSharedBots(res.data.items || []);
+  }, []);
+
+  useInterval(() => {
+    getBots();
+    getSharedBots();
+  }, 5000);
+
+  return (
+    <>
+      <Tabs defaultValue="my_bots">
+        <div className="flex flex-row items-center">
+          <div className="flex flex-row gap-2">
+            <TabsList>
+              <TabsTrigger value="my_bots">{page_bot('my_bots')}</TabsTrigger>
+              <TabsTrigger value="shared_bots">
+                {page_bot('shared_bots')}
+              </TabsTrigger>
+            </TabsList>
+            <Input
+              placeholder={page_bot('search')}
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.currentTarget.value)}
+            />
+          </div>
+          <div className="ml-auto flex items-center gap-2">
+            <Button asChild>
+              <Link href="/bots/new">
+                <Plus /> {page_bot('new_bot')}
+              </Link>
+            </Button>
+          </div>
         </div>
-      ) : (
-        <div className="sm:grid-col-1 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {bots
-            .filter((bot) => {
-              if (searchValue === '') return true;
-              return (
-                bot.title?.match(new RegExp(searchValue)) ||
-                bot.description?.match(new RegExp(searchValue))
-              );
-            })
-            .map((bot) => {
-              return (
-                <Link key={bot.id} href={`/bots/${bot.id}/chats`}>
-                  <Card className="hover:bg-accent/30 cursor-pointer gap-2 rounded-md">
-                    <CardHeader className="px-4">
-                      <CardTitle className="h-5 truncate">
-                        {bot.title}
-                      </CardTitle>
-                    </CardHeader>
-                    <CardDescription className="mb-4 truncate px-4">
-                      {bot.description || page_bot('no_description_available')}
-                    </CardDescription>
-                    <CardFooter className="justify-between px-4 text-xs">
-                      <div className="text-muted-foreground">
-                        {bot.created && (
-                          <div className="flex items-center gap-2">
-                            <Calendar className="size-3" />
-                            <FormatDate datetime={new Date(bot.created)} />
-                          </div>
-                        )}
-                      </div>
-                    </CardFooter>
-                  </Card>
-                </Link>
-              );
-            })}
-        </div>
-      )}
+
+        <TabsContent value="my_bots">
+          <BotsCardList bots={bots} searchValue={searchValue} />
+        </TabsContent>
+        <TabsContent value="shared_bots">
+          <BotsCardList bots={sharedBots} searchValue={searchValue} />
+        </TabsContent>
+      </Tabs>
     </>
   );
 };
