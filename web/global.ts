819cdb9fe5e2ce78a254b81a34c6db03e82f6dde
earayu <earayu@163.com>
2026-04-23 16:47:21 +0800

    fix web evaluation i18n typings

--- web/global.ts
+++ web/global.ts
@@ -1,9 +1,17 @@
 import { TitleGenerateRequestLanguageEnum } from '@/api';
 import messages from './src/i18n/en-US.json';
+import pageBenchmarks from './src/i18n/en-US/page_benchmarks.json';
+import pageBotEvaluation from './src/i18n/en-US/page_bot_evaluation.json';
+
+const typedMessages = {
+  ...messages,
+  page_benchmarks: pageBenchmarks,
+  page_bot_evaluation: pageBotEvaluation,
+};
 
 declare module 'next-intl' {
   interface AppConfig {
-    Messages: typeof messages;
+    Messages: typeof typedMessages;
     Locale: TitleGenerateRequestLanguageEnum;
   }
 }
